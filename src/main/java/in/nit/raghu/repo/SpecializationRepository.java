579e255572e2704c552266e007ebd20fc3ee8c4f
manoj5424 <manjbhandarkar@gmail.com>
2023-01-11 19:32:59 +0530

    spec repo update

---
+++ src/main/java/in/nit/raghu/repo/SpecializationRepository.java
@@ -0,0 +1,9 @@
+package in.nit.raghu.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.nit.raghu.entity.Specialization;
+
+public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
+
+}
