27fe8d613a3bec132789eb7ddf231f9e44c9b923
manoj5424 <manjbhandarkar@gmail.com>
2023-01-15 12:25:33 +0530

    file upload using api imdbb

---
+++ src/main/resources/static/myjs/uploadImgBb.js
@@ -0,0 +1,19 @@
+function uploadFile() {
+  var file = document.getElementById("fileOb");
+  var form = new FormData();
+  form.append("image", file.files[0]);
+  var inputs = {
+    url: "https://api.imgbb.com/1/upload?key=6a16ee8023a41e530b6b8ca78e5416cb",
+    method: "POST",
+    timeout: 0,
+    processData: false,
+    mimeType: "multipart/form-data",
+    contentType: false,
+    data: form,
+  };
+
+  $.ajax(inputs).done(function (response) {
+    var job = JSON.parse(response);
+    $("#photoLoc").val(job.data.url);
+  });
+}
